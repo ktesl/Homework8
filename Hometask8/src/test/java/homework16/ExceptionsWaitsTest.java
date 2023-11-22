211d6a0922e2d64075e0e3545d71af7a7705eb45
HARLIE\Harlie <k.teslenko@xmlshop.com>
2023-11-22 18:29:18 +0200

    add hometask16

---
+++ Hometask8/src/test/java/homework16/ExceptionsWaitsTest.java
@@ -0,0 +1,87 @@
+package homework16;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ExceptionsWaitsTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void hiddenElementTest() {
+        String url = "https://the-internet.herokuapp.com/dynamic_loading/1";
+        driver.get(url);
+
+        WebElement startBtn = driver.findElement(By.xpath("//button[text()='Start']"));
+        startBtn.click();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5);
+        WebElement text = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
+
+        Assert.assertEquals(text.getText(), "Hello World!");
+    }
+
+    @Test
+    public void renderedElementTest() {
+        String url = "https://the-internet.herokuapp.com/dynamic_loading/2";
+        driver.get(url);
+
+        WebElement startBtn = driver.findElement(By.xpath("//button[text()='Start']"));
+        startBtn.click();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5);
+        WebElement text = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("finish")));
+
+        Assert.assertEquals(text.getText(), "Hello World!");
+    }
+
+    @Test
+    public void clickBtnTest() throws InterruptedException {
+        String url = "https://www.selenium.dev/selenium/web/dynamic.html";
+        int clicksCount = 0;
+        int iterations = 3;
+        driver.get(url);
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+        WebElement addBoxBtn = driver.findElement(By.id("adder"));
+        for (int i = 0; i < iterations; i++) {
+            addBoxBtn.click();
+            clicksCount++;
+
+            List<WebElement> redboxes = wait.until(ExpectedConditions.numberOfElementsToBe(By.className("redbox"), clicksCount));
+            Assert.assertEquals(clicksCount, redboxes.size());
+        }
+
+        WebElement inputBtn = driver.findElement(By.id("reveal"));
+        for (int i = 0; i < iterations; i++) {
+            inputBtn.click();
+
+            List<WebElement> inputs = wait.until(ExpectedConditions.numberOfElementsToBe(By.id("revealed"), 1));
+            Assert.assertEquals(1, inputs.size());
+        }
+    }
+}
